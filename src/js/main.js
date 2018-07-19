98b29150ea42b84a7d9a72d4e777d0577f42be7f
Connor Waslo <connorwaslo@gmail.com>
2018-07-18 21:08:28 -0700

    Contact section
    Finished contact section.

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,12 @@
 (() => {
   'use strict';
 
-  console.log('Running');
   // Page is loaded
   const objects = document.getElementsByClassName('async-img');
   Array.from(objects).map((item) => {
     // Start loading image
     const img = new Image();
     img.src = item.dataset.src;
-    console.log('Running2');
     // Once image is loaded replace the src of the HTML element
     img.onload = () => {
       item.classList.remove('async-img');
